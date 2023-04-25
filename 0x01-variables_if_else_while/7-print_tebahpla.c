0de38f636f986bbe704a8cf8edd6c9824cec88de
Homor.wunmi <omotayomowunmi@gmail.com>
2023-04-25 11:41:33 -0700

    Alphabets in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char bt;
+
+	for (bt = 'z'; le >= 'a'; le--)
+		putchar(le);
+
+	putchar ('\n');
+
+	return (0);
+}
