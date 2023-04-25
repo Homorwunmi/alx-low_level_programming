feeb5f06cd8a83899fa6d980a748e642e379feab
Homor.wunmi <omotayomowunmi@gmail.com>
2023-04-25 08:58:38 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char c,h
+	char ch
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
 
-	for (h = "A"; h <= 'Z'; h++)
-		putchar(h);
+	for (ch = "A"; ch <= 'Z'; ch++)
+		putchar(ch);
 
 	putchar('\n');
 
