a9baeed02449af3e8a9addd781176e9c492a53e3
Homor.wunmi <omotayomowunmi@gmail.com>
2023-05-05 17:48:05 -0700

    Encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,26 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
+ * leet - encodes a string into 1337
+ * @n: input
+ * Return: the value of n
  */
 
-char *cap_string(char *s)
+char *leet(char *n)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (i = 0; i < 13; i++)
+		for (j = 0; j < 10; j++)
 		{
-			if (*(s + count) == sep_words[i])
+			if (n[i] == s1[j])
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
+				n[i] = s2[j];
 			}
 		}
-		count++;
 	}
-	return (s);
+	return (n);
 }
