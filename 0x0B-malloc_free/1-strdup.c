7a06c77986444b72eedac11c39ca67f98bc98fe1
Homor.wunmi <omotayomowunmi@gmail.com>
2023-05-16 21:00:03 -0700

    Duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,5 @@
-#iclude "main.h"
-#include <stdlib.h>/
+#include "main.h"
+#include <stdlib.h>
 
 /**
  * _strdup - copies the string given as parameter
@@ -13,14 +13,14 @@ char *_strdup(char *str)
 	unsigned int i, len;
 
 	i = 0;
-	len = 0
+	len = 0;
 
 	if (str == NULL)
 		return (NULL);
 
 	while (str[len])
 		len++;
-	duplicate = malloc(sizeof(char * (len + 1));
+	duplicate = malloc(sizeof(char) * (len + 1));
 
 	if (duplicate == NULL)
 		return (NULL);
