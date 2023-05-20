5d50618835c1909f90b129883d7fd3e78cbb9978
Homor.wunmi <omotayomowunmi@gmail.com>
2023-05-19 23:11:02 -0700

    Multiples of two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,7 +9,7 @@
  *
  * Return: no return
  */
-void_is_zero(char *argv[])
+void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
 
@@ -59,7 +59,7 @@ char *_initialize_array(char *ar, int lar)
  *
  * Return: length of the number
  */
-int_checknum(char *argv[], int n)
+int _checknum(char *argv[], int n)
 {
 	int ln;
 
@@ -81,15 +81,15 @@ int_checknum(char *argv[], int n)
  *
  * Return: 0 - success
  */
-int main(int argc; char *argv[])
+int main(int argc, char *argv[])
 {
 	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	ln1 = int_checknum(argv, 1), ln2 = _checknum(argv, 2);
-	void_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\nn"), exit(98);
 	nout = _initialize_array(nout, lnout);
@@ -108,8 +108,6 @@ int main(int argc; char *argv[])
 	if (j < 0)
 	{
 		if (nout[0] != '0')
-			break;
-
 		lnout--;
 		free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
 		k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
