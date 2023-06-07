f6192e9fa28417a7daeb978b3448a6b39116f7b0
Homorwunmi <omotayomowunmi@gmail.com>
2023-06-07 11:53:12 -0700

    My first static library

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: where memory is stored
+ * @src: where memory is copied
+ * @n: number of bytes
+ *
+ * Return: copied memory with n byte changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a = 0;
+	int b = n;
+
+	for (a < b; a++;)
+	{
+		dest[a] = src[a];
+		n--;
+	}
+	return (dest);
+}
