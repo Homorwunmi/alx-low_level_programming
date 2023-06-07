38b7eee464b866019b641ccdb64e76939bc16793
Homorwunmi <omotayomowunmi@gmail.com>
2023-06-06 19:16:54 -0700

    Insertion

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new_node);
 		}
 		else
-			temp = temp->next
+			temp = temp->next;
 	}
 
 	return (NULL);
